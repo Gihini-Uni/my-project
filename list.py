946d3b1dd831ec58906e780f980a3e66d4e16fcb
Gihini-Uni <gihinilwaid@gmail.com>
2025-03-24 10:29:22 +0530

    Change input of list.py

--- list.py
+++ list.py
@@ -1,8 +1,14 @@
-list1 = ['cat', 'dog', 'bird']
+list1=[]
+while True:
+    animal=input("Enter animal name: ")
+    if animal != "":
+        list1.append(animal)
+    else:
+        break
 
 for i in list1:
     if i=="bird":
         print("The",i,"can fly in the sky")
 
     else:
-        print(i,"is a mammal")
+        print(i,"is an animal")
