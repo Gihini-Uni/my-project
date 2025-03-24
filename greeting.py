2c8b0f8cc339c3d829407fb4bd4f6af383f897b1
Gihini <gihinilwaid@gmail.com>
2025-03-24 11:11:34 +0530

    Output changes made to greeting.py

--- greeting.py
+++ greeting.py
@@ -3,7 +3,8 @@ n = int(input("Enter number: "))
 
 if n<=5:
   print ("Good Morning",name,"!")
+  print ("This is a Python test code")
   for i in range(n):
-    print("Welcome!", end=" ")
+    print("Welcome! ", end=" ")
 else: 
   print("Number should be less than 5!")
